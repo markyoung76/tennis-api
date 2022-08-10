aac1a528f0c0f0bd9a4f22f63765abc4144e262d
Mark Young <markyoung76@me.com>
2022-08-10 10:25:19 +0100

    added event listener to search button and paired with a random number generator to select different players from the API as a test. Worked successfully and highlighted an error with the profile photo size, corrected styling to display photo properly

--- app.js
+++ app.js
@@ -23,9 +23,12 @@ async function playerInfoDisplay() {
   );
   const data = await response.json();
 
-  console.log(data.data[0]);
+  const num = Math.floor(Math.random() * 100);
+  console.log(num);
+
   data.data.map(function () {
     let ph1 = document.querySelector("#ph1");
+    let ph2 = document.querySelector("#ph2");
     let ph3 = document.querySelector("#ph3");
     let ph4 = document.querySelector("#ph4");
     let ph5 = document.querySelector("#ph5");
@@ -33,29 +36,19 @@ async function playerInfoDisplay() {
     let ph7 = document.querySelector("#ph7");
     let ph8 = document.querySelector("#ph8");
     let ph9 = document.querySelector("#ph9");
-    ph1.innerHTML = `${data.data[0].ranking}`;
-    ph3.innerHTML = `${data.data[0].team.name_short}`;
-    ph4.innerHTML = `${data.data[0].team.name_full}`;
-    ph5.innerHTML = `${data.data[0].team.gender}`;
-    ph6.innerHTML = `${data.data[0].team.country}`;
-    ph7.innerHTML = `${data.data[0].type}`;
-    ph8.innerHTML = `${data.data[0].points}`;
-    ph9.innerHTML = `${data.data[0].tournaments}`;
+    ph1.innerHTML = `${data.data[num].ranking}`;
+    ph2.style.backgroundImage = `url(${data.data[num].team.logo})`;
+    ph3.innerHTML = `${data.data[num].team.name_short}`;
+    ph4.innerHTML = `${data.data[num].team.name_full}`;
+    ph5.innerHTML = `${data.data[num].team.gender}`;
+    ph6.innerHTML = `${data.data[num].team.country}`;
+    ph7.innerHTML = `${data.data[num].type}`;
+    ph8.innerHTML = `${data.data[num].points}`;
+    ph9.innerHTML = `${data.data[num].tournaments}`;
   });
 }
 
-async function playerProfilePhoto() {
-  const response = await fetch(
-    `https://sportscore1.p.rapidapi.com/tennis-rankings/${type}?page=1`,
-    options
-  );
-  const data = await response.json();
-
-  console.log(data.data[0].team.logo);
-  let ph2 = document.querySelector("#ph2");
-  ph2.style.backgroundImage = `url(${data.data[0].team.logo})`;
-}
+let button = document.querySelector("#search-btn");
+button.addEventListener("click", playerInfoDisplay);
 
 playerInfoDisplay();
-
-playerProfilePhoto();
